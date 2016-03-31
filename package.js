4b9c43fe4541f0413518d5db0918352efbc28ffc
Abigail Watson <awatson1978@gmail.com>
2016-03-30 23:05:53 -0700

    updates

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-practitioner',
-  version: '1.1.0',
+  version: '1.1.1',
   summary: 'HL7 FHIR Resource - Practitioner',
   git: 'https://github.com/clinical-meteor/hl7-resource-practitioner',
   documentation: 'README.md'
