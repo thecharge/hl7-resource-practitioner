f45616a549da212ca6bcadb4bf9d7cfe20640138
Abigail Watson <awatson1978@gmail.com>
2016-03-30 23:12:06 -0700

    bump datatypes dep

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-practitioner',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'HL7 FHIR Resource - Practitioner',
   git: 'https://github.com/clinical-meteor/hl7-resource-practitioner',
   documentation: 'README.md'
@@ -14,7 +14,7 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.3.3');
 
-  api.use('clinical:hl7-resource-datatypes@0.2.2');
+  api.use('clinical:hl7-resource-datatypes@0.3.0');
   api.use('simple:json-routes@2.1.0');
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
